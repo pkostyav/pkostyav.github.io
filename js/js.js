d833502f16095a5be0cdcdbb2a605149fe7692f4
Kostya <SMKostya8@gmail.com>
2017-09-03 11:31:52 +0300

    first commit

---
+++ js/js.js
@@ -0,0 +1,101 @@
+function changeImage() {
+  var image = document.getElementById("changeimage");
+  
+  if (image.src.match("l2")) {
+    image.src = "image/Martin-Garrix-Your-EDM.jpg";
+  } 
+    else { 
+    image.src = "image/Martin-Garrix-Your-EDM.jpg";   
+  }  
+}
+function changeImage_2() {
+  var image = document.getElementById("changeimage");
+  
+  if (image.src.match("l2")) {
+    image.src = "image/champshow.jpg";
+  } 
+    else { 
+    image.src = "image/champshow.jpg";   
+  }  
+}
+function changeImage_3() {
+  var image = document.getElementById("changeimage");
+  
+  if (image.src.match("Concert(1)")) {
+    image.src = "image/ivb.jpg";
+  } 
+    else { 
+    image.src = "image/ivb.jpg";   
+  }  
+}
+/*player img change*/
+
+function changePlayerImage() {
+  var image = document.getElementById("zoom");
+  
+  if (image.src.match("imgonline-com-ua-PicOnPic-stH1eNq5ia35h")) {
+    image.src = "image/MGC.jpg";
+  } 
+    else { 
+    image.src = "image/imgonline-com-ua-PicOnPic-stH1eNq5ia35h.png";   
+  }  
+}
+function changePlayerImage2() {
+  var image = document.getElementById("zoom1");
+  
+  if (image.src.match("imgonline-com-ua-PicOnPic-bnXNTMJqRKdM")) {
+    image.src = "image/LFM.jpg";
+  } 
+    else { 
+    image.src = "image/imgonline-com-ua-PicOnPic-bnXNTMJqRKdM.png";   
+  }  
+}
+function changePlayerImage3() {
+  var image = document.getElementById("zoom2");
+  
+  if (image.src.match("imgonline-com-ua-PicOnPic-BnohZ1dW2epHd")) {
+    image.src = "image/images.jpg";
+  } 
+    else { 
+    image.src = "image/imgonline-com-ua-PicOnPic-BnohZ1dW2epHd.png";   
+  }  
+}
+function changePlayerImage4() {
+  var image = document.getElementById("zoom1");
+  
+  if (image.src.match("LFM")) {
+    image.src = "image/imgonline-com-ua-PicOnPic-bnXNTMJqRKdM.png";
+  } 
+    else { 
+    image.src = "image/imgonline-com-ua-PicOnPic-bnXNTMJqRKdM.png";   
+  }  
+}
+
+/*function changePlayerImage5() {
+  var image = document.getElementById("zoom2");
+  
+  if (image.src.match("imgonline-com-ua-PicOnPic-BnohZ1dW2epHd")) {
+    image.src = "image/images.jpg";
+  } 
+    else { 
+    image.src = "image/imgonline-com-ua-PicOnPic-BnohZ1dW2epHd.png";   
+  }  
+}
+function changePlayerImage6() {
+  var image = document.getElementById("zoom2");
+  
+  if (image.src.match("")) {
+    image.src = "";
+  } 
+    else { 
+    image.src = "";   
+  }  
+}*/
+
+var activeSong;
+function stopSong(){
+  activeSong.currentTime = 0;
+  activeSong.pause();
+}
+
+
